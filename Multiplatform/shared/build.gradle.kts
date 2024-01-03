55286ce19ae76a4993ae8b471fbdfb19e7645bee
chlgkdms <haeun3882@gmail.com>
2024-01-03 20:26:12 +0900

    gradle 버전 수정

--- Multiplatform/shared/build.gradle.kts
+++ Multiplatform/shared/build.gradle.kts
@@ -30,7 +30,7 @@ kotlin {
         val commonMain by getting {
             dependencies {
                 //put your multiplatform dependencies here
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
                 implementation("io.ktor:ktor-client-core:2.3.1")
                 implementation("io.ktor:ktor-client-cio:2.3.1")
                 implementation("io.ktor:ktor-client-content-negotiation:2.3.1")
